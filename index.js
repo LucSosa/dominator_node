e96e811582ff2cc49b97958f95eb0330614b9c88
LucSosa <l.sosamachado@gmail.com>
2025-02-13 06:27:30 -0300

    Feat: Create route hello/name

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require("express");
 const server = express();
 
 server.get("/hello", (req, res) => {
+    const { name, age } = req.query;
+
     return res.json({
         title: "Hello World!",
-        message: "Send a message hello"
+        message: `Hello ${name}, how are you today?`,
+        age: age
     });
 });
+
+server.get("/hello/:name", (req, res) => {
+    const { name } = req.params;
+
+    return res.json({
+        title: "Hello World!",
+        message: `Hello ${name}, how are you today?`,
+    });
+});
+
 server.listen(3003);
